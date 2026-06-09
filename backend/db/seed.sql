13ed77f22098d22c34376009d62985594764124e
Alexandre THIONG-SION <athiongsion@gridwise.fr>
2026-06-09 16:46:07 +0200

    chore: add docker deployment configuration

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE vehicles RESTART IDENTITY;
+TRUNCATE TABLE application_documents, applications, users, vehicles RESTART IDENTITY CASCADE;
 
 INSERT INTO vehicles (brand, model, price, offer_type, is_available, image_url) VALUES
 ('Renault', 'Clio', 12000, 'purchase', true, NULL),
@@ -7,3 +7,7 @@ INSERT INTO vehicles (brand, model, price, offer_type, is_available, image_url) 
 ('Renault', 'Captur', 420, 'rental', true, NULL),
 ('Audi', 'A3', 25000, 'purchase', false, NULL),
 ('Peugeot', '308', 390, 'rental', false, NULL);
+
+INSERT INTO users (first_name, last_name, email, password_hash, role) VALUES
+('Client', 'Demo', 'client.demo@mmotors.test', '$2b$10$RH7O3aM5H1tLgCeCfIzYyuTV043RumDIta8OgeFHX9e9Jx02qtfhu', 'client'),
+('Admin', 'Demo', 'admin.demo@mmotors.test', '$2b$10$57em4ouuUXkp63mrHvdFQe2bSlytR6B51KQxhMOP6AWL8bJg2PoOO', 'admin');
